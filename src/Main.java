b07cc25bde8d3047edac68c6cbc10898f7ff2478
Aladyev Dmitriy <aladyev.dmitriy@gmail.com>
2022-07-06 14:59:16 +0300

    .

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 3000;
+        int valueMile = 20;
+        int bonus = priceTicket / valueMile;
+
+        System.out.println(bonus);
+
+    }
+}
